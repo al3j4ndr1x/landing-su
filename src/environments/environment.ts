ed3efb48a456e70b6914fe28546bea94a4078c45
al3j4ndr1x <50196531+al3j4ndr1x@users.noreply.github.com>
2019-07-26 04:37:38 -0400

    ci: fix environment variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   firebase: {
     apiKey: 'FIREBASE_API_KEY',
     authDomain: 'FIREBASE_AUTH_DOMAIN',
-    databaseURL: 'FIREBASE_DATABASE_URL',
+    databaseURL: 'https://FIREBASE_DATABASE_URL.firebaseio.com',
     projectId: 'FIREBASE_PROJECT_ID',
     storageBucket: 'FIREBASE_STORAGE_BUCKET',
     messagingSenderId: 'FIREBASE_MESSAGING_SENDER_ID',
