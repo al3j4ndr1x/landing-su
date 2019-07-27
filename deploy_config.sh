958b7423978438489470153efc2ddaed56f690c4
al3j4ndr1x <50196531+al3j4ndr1x@users.noreply.github.com>
2019-07-26 23:28:29 -0400

    ci: add set errors flags and add message to console in script

--- deploy_config.sh
+++ deploy_config.sh
@@ -4,6 +4,16 @@
 # Note that sed on OSX and sed in Linux operate different ways
 # with OSX you have to pass a dummy blank file with -i like 'sed -i "" <s command> <file>'
 
+# Set Error Flags
+# Exit script if you try to use an uninitialized variable.
+set -o nounset
+
+# Exit script if a statement returns a non-true return value.
+set -o errexit
+
+# Use the error status of the first failure, rather than that of the last item in a pipeline.
+set -o pipefail
+
 cd src/environments
 sed -i 's/\(FIREBASE_API_KEY\)/'$FIREBASE_API_KEY'/' environment.prod.ts
 sed -i 's/\(FIREBASE_AUTH_DOMAIN\)/'$FIREBASE_AUTH_DOMAIN'/' environment.prod.ts
@@ -12,4 +22,5 @@ sed -i 's/\(FIREBASE_PROJECT_ID\)/'$FIREBASE_PROJECT_ID'/' environment.prod.ts
 sed -i 's/\(FIREBASE_STORAGE_BUCKET\)/'$FIREBASE_STORAGE_BUCKET'/' environment.prod.ts
 sed -i 's/\(FIREBASE_MESSAGING_SENDER_ID\)/'$FIREBASE_MESSAGING_SENDER_ID'/' environment.prod.ts
 sed -i 's/\(FIREBASE_APP_ID\)/'$FIREBASE_APP_ID'/' environment.prod.ts
-cat environment.prod.ts
+
+echo 'Updated Environment Variables'
