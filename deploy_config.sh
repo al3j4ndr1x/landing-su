4f8018513a484f3ebc4e0abc1915bf1acb14c916
al3j4ndr1x <50196531+al3j4ndr1x@users.noreply.github.com>
2020-12-19 02:27:39 -0300

    chore: linting

--- deploy_config.sh
+++ deploy_config.sh
@@ -1,4 +1,4 @@
-# !/bin/bash
+#!/bin/bash
 # deploy_config.sh
 # This script updates Firebase Environment Variables at deployment with CircleCI
 # Note that sed on OSX and sed in Linux operate different ways
@@ -15,16 +15,16 @@ set -o errexit
 set -o pipefail
 
 cd src/environments
-sed -i 's/\(FIREBASE_API_KEY\)/'$FIREBASE_API_KEY'/' environment.prod.ts
-sed -i 's/\(FIREBASE_AUTH_DOMAIN\)/'$FIREBASE_AUTH_DOMAIN'/' environment.prod.ts
-sed -i 's/\(FIREBASE_DATABASE_URL\)/'$FIREBASE_DATABASE_URL'/' environment.prod.ts
-sed -i 's/\(FIREBASE_PROJECT_ID\)/'$FIREBASE_PROJECT_ID'/' environment.prod.ts
-sed -i 's/\(FIREBASE_STORAGE_BUCKET\)/'$FIREBASE_STORAGE_BUCKET'/' environment.prod.ts
-sed -i 's/\(FIREBASE_MESSAGING_SENDER_ID\)/'$FIREBASE_MESSAGING_SENDER_ID'/' environment.prod.ts
-sed -i 's/\(FIREBASE_APP_ID\)/'$FIREBASE_APP_ID'/' environment.prod.ts
-sed -i 's/\(FIREBASE_STAGING_API_KEY\)/'$FIREBASE_STAGING_API_KEY'/' environment.staging.ts
-sed -i 's/\(FIREBASE_STAGING_PROJECT_ID\)/'$FIREBASE_STAGING_PROJECT_ID'/' environment.staging.ts
-sed -i 's/\(FIREBASE_STAGING_MESSAGING_SENDER_ID\)/'$FIREBASE_STAGING_MESSAGING_SENDER_ID'/' environment.staging.ts
-sed -i 's/\(FIREBASE_STAGING_APP_ID\)/'$FIREBASE_STAGING_APP_ID'/' environment.staging.ts
+sed -i 's/\(FIREBASE_API_KEY\)/'"$FIREBASE_API_KEY"'/' environment.prod.ts
+sed -i 's/\(FIREBASE_AUTH_DOMAIN\)/'"$FIREBASE_AUTH_DOMAIN"'/' environment.prod.ts
+sed -i 's/\(FIREBASE_DATABASE_URL\)/'"$FIREBASE_DATABASE_URL"'/' environment.prod.ts
+sed -i 's/\(FIREBASE_PROJECT_ID\)/'"$FIREBASE_PROJECT_ID"'/' environment.prod.ts
+sed -i 's/\(FIREBASE_STORAGE_BUCKET\)/'"$FIREBASE_STORAGE_BUCKET"'/' environment.prod.ts
+sed -i 's/\(FIREBASE_MESSAGING_SENDER_ID\)/'"$FIREBASE_MESSAGING_SENDER_ID"'/' environment.prod.ts
+sed -i 's/\(FIREBASE_APP_ID\)/'"$FIREBASE_APP_ID"'/' environment.prod.ts
+sed -i 's/\(FIREBASE_STAGING_API_KEY\)/'"$FIREBASE_STAGING_API_KEY"'/' environment.staging.ts
+sed -i 's/\(FIREBASE_STAGING_PROJECT_ID\)/'"$FIREBASE_STAGING_PROJECT_ID"'/' environment.staging.ts
+sed -i 's/\(FIREBASE_STAGING_MESSAGING_SENDER_ID\)/'"$FIREBASE_STAGING_MESSAGING_SENDER_ID"'/' environment.staging.ts
+sed -i 's/\(FIREBASE_STAGING_APP_ID\)/'"$FIREBASE_STAGING_APP_ID"'/' environment.staging.ts
 
 echo 'Updated Environment Variables'
